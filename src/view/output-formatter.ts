7925c4971e7acbed56bf6373e24af79d31eb218e
Oz Anani <ozanani94@gmail.com>
2025-01-12 22:14:50 +0200

    Add checkCycles functionality

--- src/view/output-formatter.ts
+++ src/view/output-formatter.ts
@@ -3,6 +3,46 @@ import { MermaidLinkGenerator } from "./mermaid/link-generator";
 import { telemetry } from "../telemetry";
 
 export class OutputFormatter {
+    public static checkCycles(mermaidCode: string): boolean {
+        const subgraphs = this.getSubgraphs(mermaidCode);
+        const nodes = this.getNodes(mermaidCode);
+
+        for (const subgraph of subgraphs) {
+            if (nodes.has(subgraph)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static getSubgraphs(mermaidCode: string): Set<string> {
+        const items = mermaidCode.matchAll(/subgraph ([^[\n]+)/g);
+        return new Set(Array.from(items, (item) => item[1].trim()));
+    }
+
+    public static getNodes(mermaidCode: string): Set<string> {
+        const inSubgraph = mermaidCode.match(/subgraph[\s\S]+end/g);
+        const nodes = new Set<string>();
+
+        if (inSubgraph) {
+            const lines = inSubgraph[0].split("\n");
+
+            for (let line of lines) {
+                line = line.trim();
+
+                if (line === "" || line.startsWith("subgraph") || line.startsWith("end")) {
+                    continue;
+                }
+
+                const node = line.split("[")[0];
+                nodes.add(node);
+            }
+        }
+
+        return nodes;
+    }
+
     public static getDiagramFileContent(modelName: string, llmResponse: string): string {
         const mermaidBlock = this.getMermaidBlock(llmResponse);
         const mermaidCode = mermaidBlock.replace(/```mermaid|```/g, "");
